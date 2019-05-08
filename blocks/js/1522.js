c8c89cdc99d7e6843c27203fdba90656b1dccbc8
Huy <huylele1997@gmail.com>
2019-05-08 14:18:46 +0700

    up gep module

--- blocks/js/1522.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    var swiper = new Swiper('.swiper-container', {
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-    });
-});
