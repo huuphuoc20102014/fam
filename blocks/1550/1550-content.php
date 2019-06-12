8266f454ed9299fa4ad9ced8941b1f10c61d1e92
Phước <phuocth97@gmail.com>
2019-06-12 20:43:50 +0700

    up detail and add cart

--- blocks/1550/1550-content.php
+++ blocks/1550/1550-content.php
@@ -1,5 +1,4 @@
 <?php
-
 $url_host = 'http://' . $_SERVER['HTTP_HOST'];
 $pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
 $pattern_uri = '/' . $pattern_document_root . '(.*)$/';
@@ -21,75 +20,81 @@ $products = $obj_products->getProductsById($_GET['id']);
 ?>
 
 <div class="type-1550">
-	<div class="container">
-		<div class="row">
-			<div class="col-md-12 col-sm-12 col-xs-12">
-				<div class="admin-panel">
-					<div class="admin-content">
-						<div class="row">
-							<div class="col-md-6 col-sm-6 col-xs-12 border-line">
-								<div class="product-img">
-									<a href="#"><img src="./images/<?php echo $products[0]['product-images'];?>" alt="product" id="main" height="300px" width="100%"></a>
-								</div>
-								<div class="row">
-									<div class="product-gallery">
-										<a href="./images/<?php echo $products[0]['images-1'];?>" onclick="swap(this); return false;">
-											<img style="height: 70px;" src="./images/<?php echo $products[0]['images-1'];?>" alt="product" class="img-responsive"/>
-											<!-- <div class="col-md-3">
-												<img style="height: 70px;" src="<?php echo $url_path ?>/images/13-1.jpg" alt="product" class="img-responsive"/>
-											</div> -->
-										</a>
-										<a href="./images/<?php echo $products[0]['images-2'];?>" onclick="swap(this); return false;">
-											<img style="height: 70px;" src="./images/<?php echo $products[0]['images-2'];?>" alt="" class="img-responsive"/>
-											<!-- <div class="col-md-3">
-												<img style="height: 70px;" src="<?php echo $url_path ?>/images/13-2.jpg" alt="" class="img-responsive"/>
-											</div> -->
-										</a>
-										<a href="./images/<?php echo $products[0]['images-3'];?>" onclick="swap(this); return false;">
-											<img style="height: 70px;" src="./images/<?php echo $products[0]['images-3'];?>" alt=""class="img-responsive"/>
-											<!-- <div class="col-md-3">
-												<img style="height: 70px;" src="<?php echo $url_path ?>/images/13-3.jpg" alt=""class="img-responsive"/>
-											</div> -->
-										</a>
-										<a href="./images/<?php echo $products[0]['images-4'];?>" onclick="swap(this); return false;">
-											<img style="height: 70px;" src="./images/<?php echo $products[0]['images-4'];?>" alt=""class="img-responsive"/>
-											<!-- <div class="col-md-3">
-												<img style="height: 70px;" src="<?php echo $url_path ?>/images/13-4.jpg" alt=""class="img-responsive"/>
-											</div> -->
-										</a>
-									</div>
-								</div>
-								
-							</div>
-							<div class="col-md-6 col-sm-6 col-xs-12 border-line">
-								<div class="product-name">
-									<h3>
-										<a href="detail.php?id=<?php echo $products[0]['product_id']; ?>">
-											<?php
-											echo $products[0]['product-name'];
-											?>
-										</a></h3>
-									</div>
-									<div class="product-info">
-										<p class="description">
-											<?php echo $products[0]['mota'];?>
-										</p>
-										<div class="product-price">
-											<h1 class="price"><?php echo $products[0]['product-price'];?>VNĐ</h1>
-											<!-- <span class="price-tax">45.000 ₫-33%</span> -->
-										</div>
-										<br>
-										<div class="product-btnadd">
-											<a href="#" class="add-to-cart">Thêm vào giỏ hàng</a>
-											<!-- <a href="#" class="add-to-cart">Add to Wishlist</a> -->
-										</div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="admin-panel">
+                    <div class="admin-content">
+                        <div class="row">
+                            <div class="col-md-6 col-sm-6 col-xs-12 border-line">
+                                <div class="product-img">
+                                    <a href="#"><img src="./images/<?php echo $products[0]['product_images']; ?>" alt="product" id="main" height="300px" width="100%"></a>
+                                </div>
+                                <div class="row">
+                                    <div class="product-gallery">
+                                        <a href="./images/<?php echo $products[0]['images_1']; ?>" onclick="swap(this); return false;">
+                                            <img style="height: 70px;" src="./images/<?php echo $products[0]['images_1']; ?>" alt="product" class="img-responsive"/>
+                                            <!-- <div class="col-md-3">
+                                                    <img style="height: 70px;" src="<?php echo $url_path ?>/images/13-1.jpg" alt="product" class="img-responsive"/>
+                                            </div> -->
+                                        </a>
+                                        <a href="./images/<?php echo $products[0]['images_2']; ?>" onclick="swap(this); return false;">
+                                            <img style="height: 70px;" src="./images/<?php echo $products[0]['images_2']; ?>" alt="" class="img-responsive"/>
+                                            <!-- <div class="col-md-3">
+                                                    <img style="height: 70px;" src="<?php echo $url_path ?>/images/13-2.jpg" alt="" class="img-responsive"/>
+                                            </div> -->
+                                        </a>
+                                        <a href="./images/<?php echo $products[0]['images_3']; ?>" onclick="swap(this); return false;">
+                                            <img style="height: 70px;" src="./images/<?php echo $products[0]['images_3']; ?>" alt=""class="img-responsive"/>
+                                            <!-- <div class="col-md-3">
+                                                    <img style="height: 70px;" src="<?php echo $url_path ?>/images/13-3.jpg" alt=""class="img-responsive"/>
+                                            </div> -->
+                                        </a>
+                                        <a href="./images/<?php echo $products[0]['images_4']; ?>" onclick="swap(this); return false;">
+                                            <img style="height: 70px;" src="./images/<?php echo $products[0]['images_4']; ?>" alt=""class="img-responsive"/>
+                                            <!-- <div class="col-md-3">
+                                                    <img style="height: 70px;" src="<?php echo $url_path ?>/images/13-4.jpg" alt=""class="img-responsive"/>
+                                            </div> -->
+                                        </a>
+                                    </div>
+                                </div>
 
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+                            </div>
+                            <div class="col-md-6 col-sm-6 col-xs-12 border-line">
+                                <div class="product-name">
+                                    <h3>
+                                        <a href="detail.php?id=<?php echo $products[0]['product_id']; ?>">
+                                            <?php
+                                            echo $products[0]['product_name'];
+                                            ?>
+                                        </a></h3>
+                                </div>
+                                <div class="product-info">
+                                    <p class="description">
+                                        <?php echo $products[0]['mota']; ?>
+                                    </p>
+                                    <div class="product-price">
+                                        <h1 class="price"><?php echo $products[0]['product_price']; ?>VNĐ</h1>
+                                        <!-- <span class="price-tax">45.000 ₫-33%</span> -->
+                                    </div>
+                                    <br>
+                                    <div class="product-btnadd">
+                                        <button class="add_cart">
+
+                                            Thêm vào giỏ hàng
+                                        </button>
+                                        <script>
+                                            $(".add_cart").click(function () {
+                                                alert("Chức năng đang bảo trì vui lòng liên hệ trực tiếp SĐT: 0902.903.199. Hoặc chát trực tiếp với admin.");
+                                            });
+                                        </script>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- blocks/1550/js/1550.js
+++ blocks/1550/js/1550.js
@@ -1,21 +1,24 @@
-$(document).ready(function() {
+$(document).ready(function () {
+    $(".add_cart").click(function () {
+        alert("Chức năng đang bảo trì vui lòng liên hệ trực tiếp SĐT: 0902.903.199. Hoặc chát trực tiếp với admin.");
+    });
     var swiper = new Swiper('.swiper-container', {
-      effect: 'cube',
-      grabCursor: true,
-      centeredSlides: true,
-      autoplay: {
-        delay: 2000,
-        disableOnInteraction: false,
-      },
-      cubeEffect: {
-        shadow: true,
-        slideShadows: true,
-        shadowOffset: 20,
-        shadowScale: 0.94,
-      },
-      pagination: {
-        el: '.swiper-pagination',
-      },
+        effect: 'cube',
+        grabCursor: true,
+        centeredSlides: true,
+        autoplay: {
+            delay: 2000,
+            disableOnInteraction: false,
+        },
+        cubeEffect: {
+            shadow: true,
+            slideShadows: true,
+            shadowOffset: 20,
+            shadowScale: 0.94,
+        },
+        pagination: {
+            el: '.swiper-pagination',
+        },
     });
 });
 function swap(image) {
